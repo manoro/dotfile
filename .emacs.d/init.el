505779bed6b1b16fa103ac7e54700bb3019617e0
maz <crtdhmn@gmail.com>
2016-08-07 21:55:21 +0900

    commit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,5 @@
 (show-paren-mode 1)
 (setq-default tab-width 2 indent-tabls-mode nil)
+
+(add-to-list 'default-frame-alist
+                '(font ."-unknown-Ubuntu Mono-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1"))
--- .vimrc
+++ .vimrc
@@ -1,7 +1,10 @@
-"ln -s dotfiles/.vimrc .vimrc
+"seln -s dotfiles/.vimrc .vimrc
 set number
+set title
+syntax on
 set tabstop=2
 set autoindent
 set expandtab
 set showmatch
 set smarttab
+set smartindent
