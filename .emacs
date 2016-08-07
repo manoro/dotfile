4bcfcc7cb4da54a9245e7a86b9537beed8fdd131
maz <crtdhmn@gmail.com>
2016-08-07 22:07:14 +0900

    commit

--- .emacs
+++ .emacs
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat)))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,3 +3,53 @@
 
 (add-to-list 'default-frame-alist
                 '(font ."-unknown-Ubuntu Mono-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1"))
+
+;;; カーソルの点滅を止める
+(blink-cursor-mode 0)
+
+;;; 対応する括弧を光らせる。
+(show-paren-mode 1)
+;;; ウィンドウ内に収まらないときだけ括弧内も光らせる。
+(setq show-paren-style 'mixed)
+
+;; 空白や長すぎる行を視覚化する。
+(require 'whitespace)
+;; 1行が80桁を超えたら長すぎると判断する。
+(setq whitespace-line-column 80)
+(setq whitespace-style '(face              ; faceを使って視覚化する。
+                         trailing          ; 行末の空白を対象とする。
+                         lines-tail        ; 長すぎる行のうち
+                                           ; whitespace-line-column以降のみを
+                                           ; 対象とする。
+                         space-before-tab  ; タブの前にあるスペースを対象とする。
+                         space-after-tab)) ; タブの後にあるスペースを対象とする。
+;; デフォルトで視覚化を有効にする。
+(global-whitespace-mode 1)
+
+;;; 現在行を目立たせる
+(global-hl-line-mode)
+
+;;; カーソルの位置が何文字目かを表示する
+(column-number-mode t)
+
+;;; カーソルの位置が何行目かを表示する
+(line-number-mode t)
+
+;;; バックアップファイルを作らない
+(setq backup-inhibited t)
+
+;;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+;;; 履歴数を大きくする
+(setq history-length 10000)
+
+;;; ミニバッファの履歴を保存する
+(savehist-mode 1)
+
+;;; 最近開いたファイルを保存する数を増やす
+(setq recentf-max-saved-items 10000)
+
+;; emacsclientで接続できるようにする。
+;; 2011-06-14
+(server-start)
